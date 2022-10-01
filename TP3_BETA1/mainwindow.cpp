efb6368567ffd238747b58bc569742025f5ee787
Tomas Godoy <74464199+im-already-tracer@users.noreply.github.com>
2022-09-30 21:02:58 -0300

    Batalla naval Beta

---
+++ TP3_BETA1/mainwindow.cpp
@@ -0,0 +1,91 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow){
+    ui->setupUi(this);
+    esconderTodo();
+    setDefaultTexts();
+    setWindowIcon(QIcon(":/images/icon.png"));
+}
+
+MainWindow::~MainWindow(){
+    delete ui;
+}
+
+void MainWindow::setDefaultTexts(){
+    ui->listaBarcos->setPlaceholderText(QStringLiteral("Elija Barco"));
+    ui->tamanioMapa->setPlaceholderText(QStringLiteral("TAMANIO MAPA"));
+    ui->cantBarcos->setPlaceholderText(QStringLiteral("CANT BARCOS"));
+    ui->posX->setPlaceholderText(QStringLiteral("X"));
+    ui->posY->setPlaceholderText(QStringLiteral("Y"));
+    ui->listaBarcos->setCurrentIndex(-1);
+}
+
+void MainWindow::esconderTodo(){
+    ui->configJuego->hide();
+    ui->mapas->hide();
+    ui->ponerBarcos->hide();
+}
+
+void MainWindow::on_nuevoJuego_clicked(){
+    ui->nuevoJuego->hide();
+    ui->cargarPartida->hide();
+    ui->configJuego->show();
+}
+
+void MainWindow::on_jugar_clicked() {
+    ui->configJuego->hide();
+    crearMapas();
+    ui->mapas->show();
+
+    if (!this->ui->checkAleatorio->isChecked()) {
+        ui->ponerBarcos->show();
+        ui->mapaAtaque->hide();
+        //PASARLO A FUNCION COMO LO DE ABAJO
+        std::string str = "Barcos agregados: " + std::to_string(this->barcosAgregados) + " / " + std::to_string(ui->cantBarcos->text().toInt());
+        QString texto = QString::fromStdString(str);
+        ui->barcosRestantes->setText(texto);
+    }
+}
+
+void MainWindow::crearMapas(){
+    int sizeMap = ui->tamanioMapa->text().toInt();
+
+    this->buttons = new QPushButton**[sizeMap];
+    this->labels = new QLabel**[sizeMap];
+    for (int i = 0; i < sizeMap; ++i) {
+        this->buttons[i] = new QPushButton*[sizeMap];
+        this->labels[i] = new QLabel*[sizeMap];
+        for (int j = 0; j < sizeMap; ++j) {
+            QPushButton* button = new QPushButton(QString::number(i + j));
+
+            connect(button, &QPushButton::released, this,
+                    [=]() { handleButton(i, j); });
+
+            this->buttons[i][j] = button;
+            ui->attackGLayout->addWidget(button, i, j);
+
+            QLabel* label = new QLabel("-");
+            this->labels[i][j] = label;
+            ui->mapGLayout->addWidget(label, i, j);
+        }
+    }
+}
+
+void MainWindow::on_aniadirBarco_clicked(){
+    if (this->barcosAgregados + 1 == ui->cantBarcos->text().toInt()) {
+        ui->ponerBarcos->hide();
+        ui->mapaAtaque->show();
+        //ACA IRIA LO DE VALIDAR POS DEL BARCO
+    } else if (ui->posX->text().toInt() > 4 && ui->posY->text().toInt() > 5){
+        this->barcosAgregados++;
+        //ESTO PASARLO A FUNCION
+        std::string str = "Barcos agregados: " + std::to_string(this->barcosAgregados) + " / " + std::to_string(ui->cantBarcos->text().toInt());
+        QString texto = QString::fromStdString(str);
+        ui->barcosRestantes->setText(texto);
+    } else {
+        QMessageBox::information(this, "ALERTA", "Error en algun parametro\nIntente de nuevo.");
+    }
+}
+
---
+++ TP3_BETA1/mainwindow.h
@@ -0,0 +1,47 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
+#include <QMessageBox>
+#include <iostream>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+    void handleButton(int x, int y) {
+        auto button = qobject_cast<QPushButton*>(sender());
+        if (button->isEnabled()) {
+          button->setDisabled(true);
+          button->setStyleSheet(QString("color: #ff0000"));
+          std::cout << x << " " << y << "\n";
+        }
+      }
+
+    void esconderTodo();
+    void crearMapas();
+    void setDefaultTexts();
+
+private slots:
+    void on_jugar_clicked();
+
+    void on_nuevoJuego_clicked();
+
+    void on_aniadirBarco_clicked();
+
+private:
+    Ui::MainWindow* ui;
+    QPushButton*** buttons;
+    QLabel*** labels;
+    int barcosAgregados = 0;
+};
+#endif // MAINWINDOW_H
